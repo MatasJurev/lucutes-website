e6d00a0bc2ba0afc4ac53782ad5ff65ed7d1ef6b
lucutes <lukas.rusas@stud.vdu.lt>
2022-06-22 19:17:40 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ doc.ready(function(){
 			$(this.element).addClass("animated "+cssvalue);
 			$(this.element).css('opacity','1').fadeIn(2000);
 		  },
-		  offset: '77%'
+		  offset: '70%'
 		})
 
 	});
